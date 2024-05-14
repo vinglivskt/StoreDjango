39ecc5ba708f8e779938a1bbe6d0b8b1fd54865b
Vinglivskt <vinglivskt@gamil.com>
2024-05-14 14:51:32 +0300

    new commit 6

--- app/settings.py
+++ app/settings.py
@@ -37,6 +37,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.postgres',
 
      #tools
     'debug_toolbar',
@@ -83,8 +84,13 @@ WSGI_APPLICATION = 'app.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'home',
+        'USER': 'home',
+        'PASSWORD': 'home',
+        'HOST': 'localhost',
+        'PORT': '5432',
+
     }
 }
 
--- goods/templatetags/goods_tags.py
+++ goods/templatetags/goods_tags.py
@@ -1,3 +1,5 @@
+from django.utils.http import urlencode
+
 from goods.models import Categories
 from django import template
 
@@ -7,3 +9,10 @@ register = template.Library()
 @register.simple_tag
 def tag_categories():
     return Categories.objects.all()
+
+
+@register.simple_tag(takes_context=True)
+def change_params(context, **kwargs):
+    q = context['request'].GET.dict()
+    q.update(kwargs)
+    return urlencode(q)
